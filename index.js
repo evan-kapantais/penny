08a20095d3d98af3c305981e9a99eb789ee6bc47
sp-evangelos-kapantais <evangelos.kapantais@socialpoint.es>
2019-09-03 17:17:42 +0200

    api calls to alpha, improve grid

---
+++ index.js
@@ -0,0 +1,12 @@
+const key = "7VBLZ89I6SVAI9PE";
+const symbol01 = document.querySelector("#symbol01").innerText;
+const symbol01Held = 5.192;
+const symbolValue = document.querySelector("#symbol01-value");
+const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol01}&interval=10min&apikey=7VBLZ89I6SVAI9PE`;
+
+fetch(url)
+.then(data => {return data.json()})
+.then(res => {
+  console.log(res);
+  symbolValue.innerText = parseFloat(res["Global Quote"]["05. price"]) * symbol01Held.toFixed(2);
+});
