5bd094a60bb04f33800c161a06e93d5f0cbb4449
evan-kapantais <evankapantais@gmail.com>
2019-09-04 23:22:28 +0200

    summary section, tabs, work on count and nav focus

--- index.js
+++ index.js
@@ -16,3 +16,81 @@ fetch(url)
 // Init Feather Icons
 
 feather.replace();
+
+// Handle Tab Change
+
+const navButtons = document.querySelectorAll(".nav-button");
+
+navButtons.forEach(n => {
+  n.addEventListener("click", showTab);
+});
+
+function showTab() {
+  const tabs = document.querySelectorAll(".tabcontent");
+  tabs.forEach(tab => {
+    tab.style.display = "none";
+  });
+
+  const tabToShow = this.querySelector("span").innerText.toLowerCase();
+  const tab = document.querySelector(`#${tabToShow}`);
+
+  tab.style.display = "block";
+
+  console.log(tab);
+}
+
+// TODO: Format Numbers
+
+// TODO: Assets Class
+
+// TODO: Render Count Up
+
+// (function renderSavings() {
+//   let totalSavings = 1600,
+//   totalInvestments = 460.188,
+//   totalEquity = 612.87,
+//   totalWorking = 738.282;
+
+//   let savingsElem = document.querySelector("#total-savings");
+//   let InvestmentsElem = document.querySelector("#total-investments");
+//   let equityElem = document.querySelector("#total-equity");
+//   let workingElem = document.querySelector("#total-working");
+
+
+//   let savingsCount = 0;
+//   let investmentsCount = 0;
+//   let equityCount = 0;
+//   let workingCount = 0;
+
+//   setInterval(() => {
+//     if (savingsCount <= totalSavings) {
+//       savingsElem.innerText = savingsCount;
+//       savingsCount += 20;
+//     }
+//     if (investmentsCount <= Math.floor(totalInvestments)) {
+//       InvestmentsElem.innerText = investmentsCount;
+//       investmentsCount += 20;
+//     } else if (investmentsCount <= totalInvestments) {
+//       InvestmentsElem.innerText = investmentsCount;
+//       investmentsCount += .1;
+//     }
+//     if (equityCount <= totalEquity) {
+//       equityElem.innerText = equityCount;
+//       equityCount += 20;
+//     }
+//     if (workingCount <= totalWorking) {
+//       workingElem.innerText = workingCount;
+//       workingCount += 20;
+//     }  
+//   }, 20);
+// })();
+
+// Handle Nav Buttons
+
+navButtons.forEach(b => {
+  b.addEventListener("click", focusNavButton);
+});
+
+function focusNavButton() {
+
+}
