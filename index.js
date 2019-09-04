00004677cc3e663ac9c14431296afe502f78527c
sp-evangelos-kapantais <evangelos.kapantais@socialpoint.es>
2019-09-04 16:38:57 +0200

    style nav links

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Stocks API
+
 const key = "7VBLZ89I6SVAI9PE";
 const symbol01 = document.querySelector("#symbol01").innerText;
 const symbol01Amount = 5.192;
@@ -7,6 +9,10 @@ const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${sy
 fetch(url)
 .then(data => {return data.json()})
 .then(res => {
-  console.log(res);
+  // console.log(res);
   symbolValue.innerText = (parseFloat(res["Global Quote"]["05. price"]) * symbol01Amount).toFixed(3);
 });
+
+// Init Feather Icons
+
+feather.replace();
